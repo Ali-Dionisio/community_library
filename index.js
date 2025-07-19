6237b0f439e6d06c6cf45adb543e987fab5640f4
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2025-07-19 10:40:09 -0300

    feat: service, controller and routes users

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 import express from 'express'
+import UserRouters from './src/routes/user.routes.js'
 const app = express()
 
 app.use(express.json());
-const users = []
-
-
-app.post("/users", (req, res) => {
-    const body = req.body
-    users.push(body);
-    res.status(201).send("Usuário criao com sucesso!")
-});
-
-app.get('/users', (req, res) => {
-res.send({message: "Usuarios: " ,users})
-})
+app.use(UserRouters);
 
 app.listen(3000, () => {
     console.log("Servidor operante na porta 3000")
--- src/repositories/user.repositories.js
+++ src/repositories/user.repositories.js
@@ -11,7 +11,7 @@ db.run(`
 `)
 
 function createUserRepository(newUser) {
-    return new Promise((res, rej) => {
+    return new Promise((resolve, reject) => {
         const {username, email, password, avatar} = newUser;
         db.run(`
             INSERT INTO users (username, email, password, avatar)
@@ -20,9 +20,9 @@ function createUserRepository(newUser) {
         [username, email, password, avatar],
         (err) => {
             if(err) {
-                rej(err);
+                reject(err);
             } else {
-                res({message:'User creaated'})
+                resolve({id: this.lastID, ...newUser})
             }
         }
         );
@@ -30,4 +30,5 @@ function createUserRepository(newUser) {
 }
 
 export default {
-    createUserRepository}
+    createUserRepository
+}
