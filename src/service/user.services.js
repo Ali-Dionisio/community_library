8499ecfc1102d9e84083ad94ebc2b05fc5f3f96d
Alisson Almeida Dionisio da Silva <alisson.a-work@outlook.com>
2025-07-20 10:01:39 -0300

    feat: bcrypt and nodemon

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,7 +1,17 @@
 import userRepository from "../repositories/user.repositories.js";
+import bcrypet from "bcrypt";
+
 
 async function createUserService(newUser){
-    const user = await userRepository.createUserRepository(newUser);
+    const foundUser = await userRepository.findUserByEmailRepository(newUser.email);
+    if(foundUser) throw new Error("User already exists!");
+    
+    const passHash = await bcrypet.hash(newUser.password, 10);
+    const user = await userRepository.createUserRepository({
+        ...newUser,
+        password: passHash,
+    });
+    if(!user) throw new Error("Error creating User");
     return user;
 }
 
